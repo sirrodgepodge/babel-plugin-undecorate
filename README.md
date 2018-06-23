0ef0a9c1f0314d0f8e8f98c96f87a3c06d3a8837
Roger Beaman <rwbeaman@gmail.com>
2018-06-23 01:07:06 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # babel-undecorate-plugin
-remove class+method decorators for testing, but keep them in your code!
+Remove class+method decorators for testing, but keep them in your code!
+
+Decorators are quite popular and are finally on their way to being here to stay: https://github.com/tc39/proposal-decorators#69
+
+However there seem to as of yet be no good ways to access the underlying decorated classes/methods for unit tests.
+
+This package attempts to address this issue by automatically exporting a copy (not a reference) to the underlying classes+methods (see below).
+
+Can either copy all classes with decorators (with the decorators removed) or target specific decorators.
+
+*Note that this package only looks for decorators on "Export" nodes at present (so if you were to assign a decorated class to a variable and export the variable it would not work).
+
+## Example Usage
+
+### .babelrc
+```js
+{
+  plugins: [["undecorate", {
+    specifically: ["anyOldClassDecorator", "anyOldMethodDecorator"]
+  }], ...]
+}
+```
+
+### sampleFile.js
+```js
+@anyOldClassDecorator
+export default class AnyOldClass {
+  @anyOldMethodDecorator
+  method() {
+    console.log('hello');  	
+  }
+
+  @whatAboutThisGuy
+  method2() {
+  }
+}
+
+@anyOldClassDecorator
+export class AnotherClass {
+  @anyOldMethodDecorator
+  method() {
+    console.log('hello');  	
+  }
+}
+```
+#### output
+```js
+@anyOldClassDecorator
+export default class AnyOldClass {
+  @anyOldMethodDecorator
+  method() {
+    console.log('hello');  	
+  }
+
+  @whatAboutThisGuy
+  method2() {
+    console.log('hello2');
+  }
+}
+
+export class __undecorated__AnyOldClass {
+  method() {
+    console.log('hello');  	
+  }
+
+  @whatAboutThisGuy
+  method2() {
+    console.log('hello2');
+  }
+}
+
+@anyOldClassDecorator
+export class AnotherClass {
+  @anyOldMethodDecorator
+  method() {
+    console.log('hello');  	
+  }
+}
+
+export class __undecorated__AnotherClass {
+  method() {
+    console.log('hello');  	
+  }
+}
+```
+
+## Options
+### specifically <Array> (default: undefined)
+Allows only removing specific decorators (by name), if not passed in plugin will remove all decorators.
+
+### undecoratedPrefix <String> (default: "__undecorated__")
+The string which class exports that have been undecorated will be prepended with.
---
+++ index.js
@@ -0,0 +1,75 @@
+module.exports = function ({ types: t}) {
+  return {
+    name: "undecorate",
+    visitor: {
+      Program(path, state) {
+        if (process.env.NODE_ENV !== 'production') {
+          var shouldKeepDecorator = function rejectAllDecorators() { return false; }
+
+          if (state.opts.specifically) {
+            var specificallySet = state.opts.specifically.reduce(function makePseudoSet(o, decoratorToBeExcluded) {
+            	o[decoratorToBeExcluded] = true;
+            	return o;
+            }, {});
+
+            shouldKeepDecorator = function rejectSpecificDecorators(decoratorName) { return !specificallySet[decoratorName]; }
+          }
+
+          path.node.body = path.node.body.reduce(function removeClassDecorators(output, node) {
+            output.push(node);
+            if (
+              t.isExportNamedDeclaration(node)
+              ||
+              t.isExportDefaultDeclaration(node)
+            ) {
+              var modified = false;
+
+              function filterOutDecorators(decorator) {
+                var testResult = shouldKeepDecorator(decorator.expression.name);
+                if (!testResult) modified = true;
+                return testResult;
+              }
+
+              var classNode = node.declaration;
+              if (t.isClassDeclaration(classNode)) {
+                var clonedClassNode = classNode.__clone();
+
+  			        if (clonedClassNode.decorators) {
+                	clonedClassNode.decorators = clonedClassNode.decorators.filter(filterOutDecorators);
+                }
+
+                var clonedClassNodeBody = clonedClassNode.body.__clone();
+                clonedClassNode.body = clonedClassNodeBody;
+                clonedClassNodeBody.body = clonedClassNodeBody.body.map(function removeMethodDecorators(classProperty) {
+                  if (!t.isClassMethod(classProperty)) {
+                    return classProperty;
+                  } else {
+                    var clonedClassProperty = classProperty.__clone();
+                    if (clonedClassProperty.decorators) {
+                    	clonedClassProperty.decorators = clonedClassProperty.decorators.filter(filterOutDecorators);
+                    }
+                    return clonedClassProperty;
+                  }
+                });
+
+                if (modified) {
+                  var undecoratedPrefix = state.opts.undecoratedPrefix || '__undecorated__';
+
+                  clonedClassNode.id = clonedClassNode.id.__clone();
+                  clonedClassNode.id.name = undecoratedPrefix + clonedClassNode.id.name;
+
+                  var clonedExportNode = node.__clone();
+                  clonedExportNode.type = 'ExportNamedDeclaration'; // need to do named export always, can't have two default exports
+                  clonedExportNode.declaration = clonedClassNode;
+
+                	output.push(clonedExportNode);
+                }
+              }
+            }
+            return output;
+          }, []);
+        }
+      },
+    }
+  };
+}
